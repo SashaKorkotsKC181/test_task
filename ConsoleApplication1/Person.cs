8907b2d82207d19561e897fdce0d83694ed68f58
Olexandr Korkots <k.sasha.g@gmail.com>
2021-03-16 18:28:49 +0200

    data input

---
+++ ConsoleApplication1/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Person : IComparable<Person>
+    {
+        string[] mounths = new string[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        string employeeName;
+        DateTime date;
+        double hours;
+        public Person(string employeeName_, string date_, double hours_)
+        {
+            employeeName = employeeName_;
+            hours = hours_;
+            date = ConvertDate(date_);
+            
+        }
+        private DateTime ConvertDate(string date_)
+        {
+            string[] inputDate = date_.Split(' ');
+            for (int i = 0; i < mounths.Length; i++)
+            {
+                if (mounths[i] == inputDate[0])                    
+                    return new DateTime(Convert.ToInt32(inputDate[2]), i + 1, Convert.ToInt32(inputDate[1]));
+            }
+            return new DateTime();
+        }
+        public int CompareTo(Person p)
+        {
+            return -this.date.CompareTo(p.date);
+        }
+        public override string ToString()
+        {
+            return employeeName + " " + date.ToShortDateString() + " " + hours.ToString();
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> mas = new List<Person>();
+            StreamReader read = new StreamReader("acme_worksheet.csv");            
+            List<string> data = new List<string>();
+            read.ReadLine();
+            string[] input = read.ReadLine().Split(',');
+
+            while(input[0] != "")
+            {
+                mas.Add(new Person(input[0], input[1], Convert.ToDouble(input[2])));
+                try
+                {
+                    input = read.ReadLine().Split(',');
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+
+            }
+            mas.Sort();
+            Console.WriteLine(mas[263]);        
+            Console.ReadKey();
+        }
+    }
+}
